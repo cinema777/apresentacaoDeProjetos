036c9e4146192879d1b7ce10a4b09b1cd636a558
cinema777 <rodolfocfelis@gmail.com>
2022-08-26 17:06:16 -0300

    Organizando apresentação de projetos

--- 2-7dayscode.js
+++
@@ -1,5 +0,0 @@
-const nome = prompt("Qual o seu nome?");
-const idade = prompt("Quantos anos você tem?");
-const linguagem = prompt("Qual linguagem de programação você está estudando?");
-const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
-alert(msg);
